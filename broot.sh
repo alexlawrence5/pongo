1901d75c18d1f003cc0b9513aedae6d68600fa5b
Isaac <axfootballcards@gmail.com>
2026-01-09 22:11:22 +0000

    Delete broot.sh because its old stuff

--- broot.sh
+++
@@ -1,9 +0,0 @@
-echo 'Updating and upgrading APT...'
-sudo apt update
-sudo apt upgrade
-echo 'Installing dependencies...'
-sudo apt install nasm
-sudo apt install make
-echo 'Building system...'
-make
-echo 'Built.'
